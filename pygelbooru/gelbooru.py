0c9a41591c0d0e2875f494176c2ac66b53cf0ef8
Makoto <FujiMakoto@users.noreply.github.com>
2020-01-28 21:22:07 -0500

    Support for querying random images

--- pygelbooru/gelbooru.py
+++ pygelbooru/gelbooru.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
 import reprlib
-from pprint import pprint
+import xml
+from random import randint
 from typing import *
 from urllib.parse import urlparse
 
@@ -24,25 +25,26 @@ class GelbooruImage:
 
     Returns the image URL when cast to str
     """
+
     def __init__(self, payload: dict, gelbooru):
         self._gelbooru = gelbooru  # type: Gelbooru
 
-        self.id             = int(payload.get('@id'))
-        self.creator_id     = payload.get('@creator_id')
-        self.created_at     = payload.get('@created_at')
-        self.file_url       = payload.get('@file_url')
-        self.filename       = os.path.basename(urlparse(self.file_url).path)
-        self.source         = payload.get('@source') or None
-        self.hash           = payload.get('@hash')
-        self.height         = int(payload.get('@height'))
-        self.width          = int(payload.get('@width'))
-        self.rating         = payload.get('@rating')
-        self.has_sample     = payload.get('@sample')
-        self.has_comments   = payload.get('@has_comments')
-        self.has_notes      = payload.get('@has_notes')
-        self.tags           = str(payload.get('@tags')).split(' ')
-        self.change         = payload.get('@change')
-        self.directory      = payload.get('@directory')
+        self.id = int(payload.get('@id'))
+        self.creator_id = payload.get('@creator_id')
+        self.created_at = payload.get('@created_at')
+        self.file_url = payload.get('@file_url')
+        self.filename = os.path.basename(urlparse(self.file_url).path)
+        self.source = payload.get('@source') or None
+        self.hash = payload.get('@hash')
+        self.height = int(payload.get('@height'))
+        self.width = int(payload.get('@width'))
+        self.rating = payload.get('@rating')
+        self.has_sample = payload.get('@sample')
+        self.has_comments = payload.get('@has_comments')
+        self.has_notes = payload.get('@has_notes')
+        self.tags = str(payload.get('@tags')).split(' ')
+        self.change = payload.get('@change')
+        self.directory = payload.get('@directory')
 
         self._comments = []  # type: List[GelbooruComment]
 
@@ -74,13 +76,14 @@ class GelbooruTag:
 
     Returns the tag name when cast to str
     """
+
     def __init__(self, payload: dict, gelbooru):
         self._gelbooru = gelbooru  # type: Gelbooru
 
-        self.id         = int(payload.get('@id'))
-        self.name       = payload.get('@name')
-        self.count      = int(payload.get('@count', 0))
-        self.ambiguous  = payload.get('@ambiguous')
+        self.id = int(payload.get('@id'))
+        self.name = payload.get('@name')
+        self.count = int(payload.get('@count', 0))
+        self.ambiguous = payload.get('@ambiguous')
 
     def __str__(self):
         return self.name
@@ -99,16 +102,17 @@ class GelbooruComment:
 
     Returns the comment itself when cast to str
     """
+
     def __init__(self, payload: dict, gelbooru, post: Optional[GelbooruImage] = None):
         self._gelbooru = gelbooru  # type: Gelbooru
         self._post = post
 
-        self.id         = int(payload.get('@id'))
-        self.post_id    = payload.get('@post_id')
-        self.creator    = payload.get('@creator')
+        self.id = int(payload.get('@id'))
+        self.post_id = payload.get('@post_id')
+        self.creator = payload.get('@creator')
         self.creator_id = payload.get('@creator_id')
         self.created_at = payload.get('@created_at')
-        self.body       = payload.get('@body')
+        self.body = payload.get('@body')
 
     async def get_post(self):
         if self._post:
@@ -132,11 +136,11 @@ class Gelbooru:
     BASE_URL = 'https://gelbooru.com/'
 
     SORT_COUNT = 'count'
-    SORT_DATE  = 'date'
-    SORT_NAME  = 'name'
+    SORT_DATE = 'date'
+    SORT_NAME = 'name'
 
-    SORT_ASC   = 'ASC'
-    SORT_DESC  = 'DESC'
+    SORT_ASC = 'ASC'
+    SORT_DESC = 'DESC'
 
     def __init__(self, api_key: Optional[str] = None,
                  user_id: Optional[str] = None,
@@ -174,40 +178,87 @@ class Gelbooru:
 
         return GelbooruImage(payload['posts']['post'], self)
 
+    async def random_post(self, *, tags: Optional[List[str]] = None,
+                          exclude_tags: Optional[List[str]] = None) -> Optional[List[GelbooruImage]]:
+        """
+        Search for and return a single random image with the specified tags.
+
+        Args:
+            tags (list of str): A list of tags to search for
+            exclude_tags (list of str): A list of tags to EXCLUDE from search results
+
+        Returns:
+            GelbooruImage or None: Returns None if no posts are found with the specified tags.
+        """
+        endpoint = self._endpoint('post')
+        endpoint.args['limit'] = 1
+
+        # Apply basic tag formatting
+        tags = self._format_tags(tags, exclude_tags)
+        if tags:
+            endpoint.args['tags'] = ' '.join(tags)
+
+        # Run the initial query to get the number of posts available
+        payload = await self._request(str(endpoint))
+        try:
+            payload = xmltodict.parse(payload)
+        except xml.parsers.expat.ExpatError:
+            raise GelbooruException("Gelbooru returned a malformed response")
+
+        # Count is 0? We have no results to fetch then
+        count = int(payload['posts']['@count'])
+        if not count:
+            return None
+
+        # Otherwise, let's pull a random ID from the number of posts
+        offset = randint(0, min(count, 20000))
+
+        return await self.search_posts(tags=tags, exclude_tags=exclude_tags, limit=1, page=offset)
+
     async def search_posts(self, *, tags: Optional[List[str]] = None,
                            exclude_tags: Optional[List[str]] = None,
-                           limit: int = 100) -> Optional[List[GelbooruImage]]:
+                           limit: int = 100,
+                           page: int = 0) -> Union[List[GelbooruImage], GelbooruImage]:
         """
         Search for images with the optionally specified tag(s)
 
         Args:
             tags (list of str): A list of tags to search for
             exclude_tags (list of str): A list of tags to EXCLUDE from search results
-            limit (int): Limit the number of results returned. Defaults to 100.
+            limit (int): Limit the number of results returned. Defaults to 100
+            page (int): The page number
 
         Returns:
-            list of GelbooruImage
+            list of GelbooruImage or GelbooruImage: Returns a single GelbooruImage of a limit of 1 is supplied
         """
         endpoint = self._endpoint('post')
-        endpoint.args['limit']  = limit
+        endpoint.args['limit'] = limit
+        endpoint.args['pid'] = page
 
         # Apply basic tag formatting
-        tags = [tag.strip().lower().replace(' ', '_') for tag in tags] if tags else []
-        exclude_tags = ['-' + tag.strip().lstrip('-').lower().replace(' ', '_') for tag in exclude_tags] if exclude_tags else []
-
-        if tags or exclude_tags:
-            endpoint.args['tags'] = ' '.join(tags + exclude_tags)
+        tags = self._format_tags(tags, exclude_tags)
+        if tags:
+            endpoint.args['tags'] = ' '.join(tags)
 
         # Fetch and parse XML, then make sure we actually have results
         payload = await self._request(str(endpoint))
-        payload = xmltodict.parse(payload)
+        try:
+            payload = xmltodict.parse(payload)
+        except xml.parsers.expat.ExpatError:
+            raise GelbooruException("Gelbooru returned a malformed response")
         if 'posts' not in payload:
             return []
 
         # Single results are not returned as arrays/lists and need to be processed directly instead of iterated
-        return [GelbooruImage(p, self) for p in payload['posts']['post']] \
+        result = [GelbooruImage(p, self) for p in payload['posts']['post']] \
             if isinstance(payload['posts']['post'], list) \
             else [GelbooruImage(payload['posts']['post'], self)]
+
+        # Return the first result if we have a limit of 1 explicitly set
+        if limit == 1:
+            return result[0]
+        else:
+            return result
 
     async def tag_list(self, *, name: Union[str, List[str], None] = None,
                        name_pattern: Optional[str] = None,
@@ -220,7 +271,7 @@ class Gelbooru:
         Args:
             name (str or list of str): A single tag name to query or a list of tags
             name_pattern (str): A wildcard search for your query using LIKE. (choolgirl would act as *choolgirl* wildcard search.) Cannot be used with names.
-            limit (int): Limit the number of results returned. Defaults to 100.
+            limit (int): Limit the number of results returned. Defaults to 100
             sort_by (): Sort by either SORT_COUNT (tag usage count), SORT_NAME, or SORT_DATE
             sort_order (): Sort order; either SORT_ASC or SORT_DESC
 
@@ -313,6 +364,24 @@ class Gelbooru:
             endpoint.args['user_id'] = self._user_id
 
         return endpoint
+
+    def _format_tags(self, tags: list, exclude_tags: list):
+        """
+        Apply basic tag formatting
+
+        Args:
+            tags (list of str): A list of tags to search for
+            exclude_tags (list of str): A list of tags to EXCLUDE from search results
+
+        Returns:
+            list of str
+        """
+        # Apply basic tag formatting
+        tags = [tag.strip().lower().replace(' ', '_') for tag in tags] if tags else []
+        exclude_tags = ['-' + tag.strip().lstrip('-').lower().replace(' ', '_') for tag in
+                        exclude_tags] if exclude_tags else []
+
+        return tags + exclude_tags
 
     async def _request(self, url: str) -> bytes:
         async with aiohttp.ClientSession(loop=self._loop) as session:
